c9b99e424ccc3de6a023f43c7f2182d22a20d9b9
Didrik Mikkelsen <didrikmikkelsen.92@gmail.com>
2023-04-27 20:55:29 +0200

    Finished the second fucntion in Rock, Paper and Scissor project

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 
 
 function getComputerChoice() {
-  const randomWords = ["Rock", "Paper", "Scissors"];
+  const randomWords = ["rock", "paper", "scissors"];
   let choice = "";
   
   choice = randomWords[Math.floor(Math.random() * randomWords.length)]
@@ -11,10 +11,43 @@ function getComputerChoice() {
 
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 
 
 function playRound(playerSelection, computerSelection) {
-    
+
+computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
 }
+else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+}
+
+else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+}
+
+else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+}
+
+else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+}
+
+else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+}
+
+else {
+    return "Draw";
+}
+
+}
+
+
+console.log(playRound("Paper"));
